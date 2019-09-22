cfb90654328f13d60f92ab7f786dafb1fbcb15f2
Kuldeep Singh <kuldeep.singh2@hpe.com>
2019-09-22 20:26:41 +0530

    Configured authorization for API endpoints. Steps: 1. You should have already setup a way of authentication for your application before setting up authorization. 2. Create a class extending WebSecurityConfigurerAdapter and override configure(HttpSecurity http) method. You can write your authorization rules in this method. You can use ant style wild card syntax for API endpoints to map URLs to specific roles.

--- src/main/java/com/example/SecurityConfiguration.java
+++ src/main/java/com/example/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,11 +19,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("boot")
                 .password("boot")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
                 .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+
     }
 }
