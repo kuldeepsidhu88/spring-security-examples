87db6f6cb967a8a8cbdc79d5d1467a4a3f207ca4
Kuldeep Singh <kuldeep.singh2@hpe.com>
2019-10-02 11:41:22 +0530

    Add jpa support for authentication using custom UserDetailsService. Steps: 1. Create an entity for user mapping to user table in database. 2. Create a repository interface for user entity. Add method for finding user by username. 3. Call UserRepository from custom UserDetailsService for fetching user by username. Build the UserDetails object from user details. 4. Configure database credentials in application.properties file.

--- src/main/java/com/example/model/MyUserDetails.java
+++ src/main/java/com/example/model/MyUserDetails.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.model;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -6,23 +6,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
 
     private String username;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
 
-    public MyUserDetails(String username) {
-        this.username = username;
+    public MyUserDetails(User user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.active = user.isActive();
+        this.authorities = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "pass";
+        return password;
     }
 
     @Override
@@ -47,6 +57,6 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return active;
     }
 }
